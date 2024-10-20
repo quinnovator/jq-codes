421b49c0dc5072fad64fbe7a1ac72cc30f49b1f9
Jack Quinn <jack@jq.codes>
2024-10-19 20:50:24 -0400

    Release V1

--- sst.config.ts
+++ sst.config.ts
@@ -1,15 +1,14 @@
 /// <reference path="./.sst/platform/config.d.ts" />
-
 const APPLICATION_KEY = 'jq-codes-web';
 const PROD_STAGE = 'main';
 const EXTERNAL_URL = 'jq.codes';
-
 export default $config({
   app(input) {
     return {
       name: APPLICATION_KEY,
       removal: input?.stage === PROD_STAGE ? 'retain' : 'remove',
       home: 'aws',
+      providers: { '@pulumiverse/vercel': '1.14.3' },
     };
   },
   async run() {
@@ -18,6 +17,9 @@ export default $config({
         domain: {
           name: EXTERNAL_URL,
           redirects: [`www.${EXTERNAL_URL}`],
+          dns: sst.vercel.dns({
+            domain: EXTERNAL_URL,
+          }),
         },
       }),
     });
